cb0a4490c992a280247cc6fbed4e40c7e1afb0e0
Semyon Lopatkin <slem.vs@gmail.com>
2017-03-19 17:00:54 +0700

    Change version to 0.0.4

--- .build/VersionInfo.cs
+++ .build/VersionInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.3")]
-[assembly: AssemblyFileVersion("0.0.3")]
+[assembly: AssemblyVersion("0.0.4")]
+[assembly: AssemblyFileVersion("0.0.4")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
