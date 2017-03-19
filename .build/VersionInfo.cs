0aecba84227488ee1cf506816e75d7a56b787d02
Semyon Lopatkin <slem.vs@gmail.com>
2017-03-19 13:49:39 +0700

    change version to 0.0.2

--- .build/VersionInfo.cs
+++ .build/VersionInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("0.0.2")]
 [assembly: AssemblyFileVersion("0.0.2")]
