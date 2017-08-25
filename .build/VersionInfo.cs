27d4fa0c1ef876deeb85268ce9d9c5fcbc219817
Semyon Lopatkin <s.lopatkin@eastbanctech.ru>
2017-08-25 12:10:27 +0700

    Sign assemblies

--- .build/VersionInfo.cs
+++ .build/VersionInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 
-[assembly: AssemblyVersion("0.0.5")]
-[assembly: AssemblyFileVersion("0.0.5")]
+[assembly: AssemblyVersion("0.0.8")]
+[assembly: AssemblyFileVersion("0.0.8")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
